76c1e494a134a1af4cf76fe40cb7aef02173124d
Cerulan <cerulan@cerulan.net>
2016-11-26 17:43:11 -0500

    some new items

--- src/main/java/net/cerulan/healthhungertweaks/item/ModItems.java
+++ src/main/java/net/cerulan/healthhungertweaks/item/ModItems.java
@@ -6,9 +6,11 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModItems {
 
 	public static ItemBase itemHealthKit;
+	public static ItemBase itemWoodFiberBandage;
 	
 	public static void init() {
 		itemHealthKit = register(new ItemHealthKit("healthKit").setCreativeTab(HealthHungerTweaks.sidedProxy.creativeTab));
+		itemWoodFiberBandage = register(new ItemBase("woodFiberBandage").setCreativeTab(HealthHungerTweaks.sidedProxy.creativeTab));
 	}
 
 	private static <T extends Item> T register(T item) {
