67ee2afb3ac574a7e0ab4c08ca3dd5c3e61f9777
Cerulan <cerulan@cerulan.net>
2016-11-26 19:45:43 -0500

    All kinds of crafting materials and their recipes

--- src/main/java/net/cerulan/healthhungertweaks/item/ItemHealthKit.java
+++ src/main/java/net/cerulan/healthhungertweaks/item/ItemHealthKit.java
@@ -1,19 +1,9 @@
 package net.cerulan.healthhungertweaks.item;
 
-import java.util.List;
-
-import net.cerulan.healthhungertweaks.HealthHungerTweaks;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
 public class ItemHealthKit extends ItemBase {
 
 	public ItemHealthKit(String name) {
 		super(name);
-		this.setSubtypeAmount(3);
 	}
 
 }
